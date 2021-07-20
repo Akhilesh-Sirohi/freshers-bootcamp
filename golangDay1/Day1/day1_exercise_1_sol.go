8fda507ef65f201578694a7463bbea25da283010
Akhilesh-Sirohi <akhileshsirohii@gmail.com>
2021-07-20 17:04:55 +0530

    solved first problem

--- golangDay1/Day1/day1_exercise_1_sol.go
+++ golangDay1/Day1/day1_exercise_1_sol.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -12,6 +11,13 @@ type matrix struct {
 	//elements:=make([][]int, number_of_rows,number_of_cols)
 }
 
+func createMatrix(number_of_rows int, number_of_cols int) matrix{
+	elements:= make([][]int, number_of_rows);
+	for i:=0;i<number_of_rows;i++{
+		elements[i]= make([] int,number_of_cols);
+	}
+	return matrix{number_of_rows,number_of_cols,elements}
+}
 func getRows(name matrix) int{
 	return name.number_of_rows;
 }
@@ -21,28 +27,91 @@ func getColumns(name matrix) int{
 func setElement(name matrix,i int, j int, value int) {
 	name.elements[i][j]=value;
 }
-func addMarix(name1 matrix, name2 matrix) matrix{
+func addMatrix(name1 matrix, name2 matrix) matrix{
+	ans:= matrix{0,0,make ([][]int,1)}
 	if getRows(name1)!=getRows(name2) || getColumns(name1)!=getColumns(name2) {
 		//errors.New constructs a basic error value with the given error message.
-		errors.New("Can't add both matrix as their dimensions are different")
+		fmt.Println("Failed: Can't add both matrix as their dimensions are different")
+		//return ans,errors.New("Can't add both matrix as their dimensions are different")
 	} else {
-		ans := name1
+		ans = name1
 		for i := 0; i < name1.number_of_rows; i++ {
 			for j := 0; j < name1.number_of_cols; j++ {
 				ans.elements[i][j] = name1.elements[i][j] + name2.elements[i][j]
 			}
 		}
-		return ans;
 	}
-	ans:= matrix{0,0,make ([][]int,1)}
+	return ans;
+
 	return ans
 }
 func main() {
-	x := matrix{15, 16, make([][]int, 3)}
+	x := createMatrix(4,5);
 	//setElement(x,1,1,12)
-	y := matrix{16, 17, make([][]int, 3)}
+	y := createMatrix(4,5);
+	setElement(x,0,0,12);
+	setElement(y,0,0,13);
 	fmt.Println(getRows(x))
 	fmt.Println(getColumns(x))
 	fmt.Println(x.elements)
-	fmt.Println(addMarix(x,y))
+	fmt.Println(addMatrix(x,y))
+	fmt.Println(addMatrix(createMatrix(4,5),createMatrix(6,3)))
+}package main
+
+import (
+	"fmt"
+)
+
+type matrix struct {
+	number_of_rows int
+	number_of_cols int
+	elements [][]int
+	//elements:=make([][]int, number_of_rows,number_of_cols)
+}
+
+func createMatrix(number_of_rows int, number_of_cols int) matrix{
+	elements:= make([][]int, number_of_rows);
+	for i:=0;i<number_of_rows;i++{
+		elements[i]= make([] int,number_of_cols);
+	}
+	return matrix{number_of_rows,number_of_cols,elements}
+}
+func getRows(name matrix) int{
+	return name.number_of_rows;
+}
+func getColumns(name matrix) int{
+	return name.number_of_cols;
+}
+func setElement(name matrix,i int, j int, value int) {
+	name.elements[i][j]=value;
+}
+func addMatrix(name1 matrix, name2 matrix) matrix{
+	ans:= matrix{0,0,make ([][]int,1)}
+	if getRows(name1)!=getRows(name2) || getColumns(name1)!=getColumns(name2) {
+		//errors.New constructs a basic error value with the given error message.
+		fmt.Println("Failed: Can't add both matrix as their dimensions are different")
+		//return ans,errors.New("Can't add both matrix as their dimensions are different")
+	} else {
+		ans = name1
+		for i := 0; i < name1.number_of_rows; i++ {
+			for j := 0; j < name1.number_of_cols; j++ {
+				ans.elements[i][j] = name1.elements[i][j] + name2.elements[i][j]
+			}
+		}
+	}
+	return ans;
+
+	return ans
+}
+func main() {
+	x := createMatrix(4,5);
+	//setElement(x,1,1,12)
+	y := createMatrix(4,5);
+	setElement(x,0,0,12);
+	setElement(y,0,0,13);
+	fmt.Println(getRows(x))
+	fmt.Println(getColumns(x))
+	fmt.Println(x.elements)
+	fmt.Println(addMatrix(x,y))
+	fmt.Println(addMatrix(createMatrix(4,5),createMatrix(6,3)))
 }
