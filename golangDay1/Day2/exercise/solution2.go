bd6fcff8e54cd623ff4b3117c17b5f5ff078a456
Akhilesh-Sirohi <akhileshsirohii@gmail.com>
2021-07-22 16:34:40 +0530

    solution 2 complete

--- golangDay1/Day2/exercise/solution2.go
+++ golangDay1/Day2/exercise/solution2.go
@@ -7,25 +7,24 @@ import (
 	"time"
 )
 
-var rating int
+var total_rating int =0;
 func getRating(student int,wg *sync.WaitGroup) {
 	defer wg.Done()
 	//x:=rand.Intn(10)
 	time.Sleep(1*time.Second)
-	rating= rand.Intn(10)
-	//fmt.Println(student," X ",rating);
+	rating:= rand.Intn(10)
+	total_rating+=rating
+	fmt.Println("rating given by student ",student," is ",rating);
 }
 
 func main() {
 	var wg sync.WaitGroup
-	total_rating:=0
+
 	total_student:=5
 	for student := 1;  student<= total_student; student++ {
 		wg.Add(1)
-		go getRating(student,&wg)
-		wg.Wait()
-		total_rating+=rating;
-		fmt.Println(student," ",rating);
+		go getRating(student,&wg);
 	}
+	wg.Wait()
 	fmt.Println("average rating on scale of 10 is: ", total_rating/total_student)
 }
