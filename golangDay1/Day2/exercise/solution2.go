a87f83f2813cd1a4795960e8d013e4bf2533b881
Akhilesh-Sirohi <akhileshsirohii@gmail.com>
2021-07-22 16:21:30 +0530

    solution 2 (why is it slow)

--- golangDay1/Day2/exercise/solution2.go
+++ golangDay1/Day2/exercise/solution2.go
@@ -7,26 +7,25 @@ import (
 	"time"
 )
 
-var total_rating int =0
-func getRating(student int ,wg *sync.WaitGroup) {
+var rating int
+func getRating(student int,wg *sync.WaitGroup) {
 	defer wg.Done()
 	//x:=rand.Intn(10)
 	time.Sleep(1*time.Second)
-	rating:= rand.Intn(10)
-	fmt.Println(student," ",rating);
-	total_rating+=rating
+	rating= rand.Intn(10)
+	//fmt.Println(student," X ",rating);
 }
 
 func main() {
-
 	var wg sync.WaitGroup
-
-	for student := 1;  student<= 5; student++ {
+	total_rating:=0
+	total_student:=5
+	for student := 1;  student<= total_student; student++ {
 		wg.Add(1)
 		go getRating(student,&wg)
-		//total_rating+=rating;
+		wg.Wait()
+		total_rating+=rating;
+		fmt.Println(student," ",rating);
 	}
-
-	wg.Wait()
-	fmt.Println(total_rating)
+	fmt.Println("average rating on scale of 10 is: ", total_rating/total_student)
 }
